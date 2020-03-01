61d1831f6e17694e4932ded5b2e1fdacf395e06d
chesiree-balogh <chezarium04@gmail.com>
2020-02-29 22:34:44 -0500

    explorer mode

--- Program.cs
+++ Program.cs
@@ -42,14 +42,15 @@ namespace rhythm
       }
 
 
-
       {
         Console.WriteLine("Welcome to Rhythm!");
         PopulateDatabase();
 
+
         var newBand = new Band();
         var newAlbum = new Album();
         var newSong = new Song();
+
 
         var isRunning = true;
         while (isRunning)
@@ -61,8 +62,11 @@ namespace rhythm
           Console.WriteLine($"Add a (B)and, (P)roduce, (L)et a band go, (R)esign a band, (V)iew, or (Q)uit");
           var input = Console.ReadLine().ToLower();
 
+
           if (input == "b")
           {
+            //add list of current bands
+
             Console.WriteLine($"Whats the bands name?");
             newBand.BandName = Console.ReadLine().ToLower();
 
@@ -97,6 +101,8 @@ namespace rhythm
             var whatToProduce = Console.ReadLine().ToLower();
             if (whatToProduce == "a")
             {
+              //add list of current bands with band id's
+
               Console.WriteLine($"Which band ID produced this album?");
               newAlbum.BandId = int.Parse(Console.ReadLine().ToLower());
 
@@ -106,7 +112,7 @@ namespace rhythm
               Console.WriteLine($"Is it explicit, (True) or (False)?");
               newAlbum.IsExplicit = bool.Parse(Console.ReadLine().ToLower());
 
-              Console.WriteLine($"When was it released?");
+              Console.WriteLine($"When was it released? YYYY-MM-DD");
               newAlbum.ReleaseDate = DateTime.Parse(Console.ReadLine());
 
               db.Albums.Add(newAlbum);
@@ -114,6 +120,8 @@ namespace rhythm
             }
             else if (whatToProduce == "s")
             {
+              //add list of current albums with album id's
+
               Console.WriteLine($"Which album ID does this song belong to?");
               newSong.AlbumId = int.Parse(Console.ReadLine().ToLower());
 
@@ -134,8 +142,11 @@ namespace rhythm
             }
           }
 
+
           else if (input == "l")
           {
+            //list of current signed bands
+
             Console.WriteLine($"Which band name would you like to un-sign?");
             var unSign = Console.ReadLine().ToLower();
 
@@ -147,6 +158,8 @@ namespace rhythm
 
           else if (input == "r")
           {
+            //list of current unsigned bands
+
             Console.WriteLine($"Which band name would you like to re-sign?");
             var reSign = Console.ReadLine().ToLower();
 
@@ -155,10 +168,74 @@ namespace rhythm
             db.SaveChanges();
           }
 
-          // else if (input == "v")
-          // {
 
-          // }
+          else if (input == "v")
+          {
+            Console.WriteLine($" What would you like to view?");
+            Console.WriteLine($"All albums by (band), all albums by (release) date, ");
+            Console.WriteLine($"all (songs) on a album, all (signed) bands, all (unsigned) bands.");
+            var viewInput = Console.ReadLine().ToLower();
+
+            //view all albums for a band
+            if (viewInput == "band")
+            {
+              Console.WriteLine($"Which bands albums would you like to see?");
+              var whichBand = Console.ReadLine().ToLower();
+
+              var findingBand = db.Bands.FirstOrDefault(w => w.BandName == whichBand);
+              var listOfAlbums = db.Albums.Where(w => w.BandId == findingBand.Id);
+
+              Console.WriteLine($"List of albums produced by {findingBand.BandName}: ");
+              foreach (var albums in listOfAlbums)
+              {
+                Console.WriteLine($"{albums.Title}");
+              }
+            }
+            //view all albums, by release date
+            else if (viewInput == "release")
+            {
+              var releaseDate = db.Albums.OrderBy(o => o.ReleaseDate);
+              foreach (var release in releaseDate)
+              {
+                Console.WriteLine($" {release.ReleaseDate}");
+              }
+            }
+            // //view all of a albums song
+            else if (viewInput == "songs")
+            {
+              Console.WriteLine($"Which album would you like to view list of songs from?");
+              var whichAlbum = Console.ReadLine().ToLower();
+
+              var findingAlbum = db.Albums.FirstOrDefault(w => w.Title == whichAlbum);
+              var songsInAlbum = db.Songs.Where(w => w.AlbumId == findingAlbum.Id);
+
+              Console.WriteLine($"Songs on album {whichAlbum}: ");
+              foreach (var songs in songsInAlbum)
+              {
+                Console.WriteLine($"{songs.Title}");
+              }
+            }
+            //view all bands that are signed
+            else if (viewInput == "signed")
+            {
+              var bandIsSigned = db.Bands.Where(b => b.IsSigned == true);
+              foreach (var signed in bandIsSigned)
+              {
+                Console.WriteLine($" Current signed bands: {signed.BandName}");
+              }
+            }
+            // //view all bands that are not signed
+            else if (viewInput == "unsigned")
+            {
+              var bandNotSigned = db.Bands.Where(b => b.IsSigned == false);
+              foreach (var signed in bandNotSigned)
+              {
+                Console.WriteLine($" Current un-signed bands: {signed.BandName}");
+              }
+            }
+          }
+
+
           else if (input == "q")
           {
             isRunning = false;
